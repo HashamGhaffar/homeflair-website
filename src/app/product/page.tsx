3530c378453087959cf87792e25a69a3f40bf11b
Marsad Ghanvi <marsad11223@gmail.com>
2025-02-17 22:45:22 +0500

    fix build issues

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -6,14 +6,17 @@ import Navbar from "@/_components/header/Navbar";
 import HeroProduct from "./_sections/HeroProduct";
 import Footer from "@/_components/Footer";
 import RezultForItem from "./_sections/RezultForItem";
+import { Suspense } from "react";
 
 export default function ProductPage() {
   return (
     <Box>
-      <Navbar />
-      <HeroProduct />
-      <RezultForItem />
-      <Footer />
+      <Suspense fallback={<div>Loading product details...</div>}>
+        <Navbar />
+        <HeroProduct />
+        <RezultForItem />
+        <Footer />
+      </Suspense>
     </Box>
   );
 }
